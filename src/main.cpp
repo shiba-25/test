961f3d617cf824693f562c161986e85a68f60f6f
shiba-25 <chestnut9165@gmail.com>
2025-06-26 16:12:37 +0900

    a

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "Mbed.h"
+BufferedSerial pc(USBTX,USBRX,115200);
+DigitalIn button0(PA_0,PullUp);
+
+int main() {
+    int read1;
+    while (1)
+    {
+        read1 = button0.read();
+        if (read1 == 0) {
+            printf("Hello\n");//変更しました
+        }
+    }  
+}
